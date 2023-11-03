9cf783a4a844ea3340c09885cec96fca63ca53c2
fsampson30 <fsampson30@gmail.com>
2023-11-03 07:16:15 -0300

    Initial commit

---
+++ src/main/java/com/sampson/applicationjsp/ApplicationJspApplication.java
@@ -0,0 +1,13 @@
+package com.sampson.applicationjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampson/applicationjsp/ApplicationJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.applicationjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
