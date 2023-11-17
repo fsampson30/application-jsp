d50abdff669082138b86b1f7901f114f4f6b8334
fsampson30 <fsampson30@gmail.com>
2023-11-17 07:37:00 -0300

    Created new view to Add Todo and did the navigation between views.

--- src/main/java/com/sampson/applicationjsp/todo/TodoController.java
+++ src/main/java/com/sampson/applicationjsp/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.sampson.applicationjsp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -22,5 +23,15 @@ public class TodoController {
         List<Todo> todos = todoService.findByUsername("flavio");
         modelMap.addAttribute("todos",todos);
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage(){
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodoPage(){
+        return "redirect:list-todos";
     }
 }
