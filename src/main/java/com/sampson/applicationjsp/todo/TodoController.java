9629da330447119641d9bf8da87524716fcdcb01
fsampson30 <fsampson30@gmail.com>
2023-11-20 07:39:00 -0300

    Refactored to add Server validation.

--- src/main/java/com/sampson/applicationjsp/todo/TodoController.java
+++ src/main/java/com/sampson/applicationjsp/todo/TodoController.java
@@ -29,14 +29,17 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage(){
+    public String showNewTodoPage(ModelMap modelMap){
+        String username = (String)modelMap.get("name");
+        Todo todo = new Todo(0,username,"",LocalDate.now().plusYears(1),false);
+        modelMap.put("todo",todo);
         return "todo";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodoPage(@RequestParam String description, ModelMap modelMap){
+    public String addNewTodoPage(ModelMap modelMap, Todo todo){
         String username = (String)modelMap.get("name");
-        todoService.addTodo(username, description, LocalDate.now().plusYears(1),false );
+        todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1),false );
         return "redirect:list-todos";
     }
 }
