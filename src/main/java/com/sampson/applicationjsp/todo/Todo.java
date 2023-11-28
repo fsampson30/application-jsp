7bdbb41428b209787e60528650e54047e31b8e07
fsampson30 <fsampson30@gmail.com>
2023-11-28 07:51:05 -0300

    Created and populated Todo table.

--- src/main/java/com/sampson/applicationjsp/todo/Todo.java
+++ src/main/java/com/sampson/applicationjsp/todo/Todo.java
@@ -1,11 +1,17 @@
 package com.sampson.applicationjsp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
     @Size(min = 10, message = "Enter 10 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE) values(10001,'flavio','Learn AWS Certified',CURRENT_DATE,false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE) values(10002,'flavio','Learn Docker Certified',CURRENT_DATE,false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE) values(10003,'flavio','Learn Google Certified',CURRENT_DATE,false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE) values(10004,'flavio','Learn Oracle Certified',CURRENT_DATE,false);
